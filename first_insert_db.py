2d106ff6a34df71fb2fcb4bae7a88192dd55a8e1
Gabriel Bastos <gabriel.bastos@poli.ufrj.br>
2018-11-22 00:27:06 -0200

    Arquivo de configuracao do bd e arquivo de insercao inicial

---
+++ first_insert_db.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from classes import Base, All_people, Pessoa, Usuario
+
+#engine = create_engine("mysql+mysqldb://root:password@localhost/app_proximo")
+engine = create_engine('mysql+mysqldb://so2018:bastoseleleo123@so2018.mysql.pythonanywhere-services.com/so2018$default')
+
+
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+#Truncando tabelas p/ sobrescrever dados
+session.execute('''SET FOREIGN_KEY_CHECKS = 0''')
+session.execute('''TRUNCATE TABLE all_people''')
+session.execute('''TRUNCATE TABLE usuario''')
+session.execute('''TRUNCATE TABLE pessoa''')
+session.execute('''SET FOREIGN_KEY_CHECKS = 1''')
+session.commit()
+session.close()
+
+
+#Inserindo os dados
+
+admin = Usuario(name="admin",password="password")
+session.add(admin)
+session.commit()
+
+
+aluno1 = All_people(nome="Zé das Couves",dre="12345678")
+session.add(aluno1)
+session.commit()
+
+
+testeTabela = Pessoa(nome="Zé das Couves", dre = aluno1.dre, hora_chegada="08:00:10", hora_saida="18:00:10")
+session.add(testeTabela)
+session.commit()
+
+print ("Banco de dados completo")
---
+++ setupbd.py
@@ -0,0 +1,62 @@
+
+import sys
+from sqlalchemy import create_engine, Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Usuario(Base):
+	__tablename__ = 'usuario'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	password = Column(String(250), nullable=False)
+
+	@property
+	def serialize(self):
+		return {
+		'id': self.id,
+		'name': self.name,
+		'password': self.password,
+		}
+
+class All_people(Base):
+	__tablename__ = 'all_people'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	dre = Column(Integer, nullable=False)
+
+	@property
+	def serialize(self):
+		return {
+		'id': self.id,
+		'nome': self.name,
+		'dre': self.dre,
+		}
+
+class Pessoa(Base):
+	__tablename__ = 'pessoa'
+
+	id = Column(Integer, primary_key=True)
+	nome = Column(String(250), nullable=False)
+	dre = Column(Integer, nullable=False,ForeignKey('all_people.dre'))
+	hora_chegada = Column(String(250), nullable=False)
+	hora_saida = Column(String(250), nullable=False)
+
+	@property
+	def serialize(self):
+		return {
+		'id': self.id,
+		'nome': self.nome,
+		'dre': self.dre,
+		'hora_chegada':self.hora_chegada,
+		'hora_saida':self.hora_saida
+		}
+
+
+#engine = create_engine("mysql+mysqldb://root:password@localhost/app_proximo")
+engine = create_engine('mysql+mysqldb://so2018:bastoseleleo123@so2018.mysql.pythonanywhere-services.com/so2018$default')
+
+Base.metadata.create_all(engine)
