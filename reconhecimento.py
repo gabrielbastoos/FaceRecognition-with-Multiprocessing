a0a0b6326bdef01f34043df0730fe23dfcdb08df
leonardoafs <leonardoafs@poli.ufrj.br>
2018-12-05 02:49:42 -0200

    reconhecimento.py quaaaase funcionando

--- reconhecimento.py
+++ reconhecimento.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
+import multiprocessing
 import multiprocessing.dummy as mp
 import face_recognition
 import cv2
 import time
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-
-from setupbd import Base, All_people, Pessoa, Usuario
-
-engine = create_engine("mysql+mysqldb://root:password@localhost/so2018")
-#engine = create_engine('mysql+mysqldb://so2018:bastoseleleo123@so2018.mysql.pythonanywhere-services.com/so2018$default')
-
-
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
 
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
@@ -42,18 +28,19 @@ bastos_face_encoding = face_recognition.face_encodings(bastos_image)[0]
 # Cria um array de faces conhecidas e seus nomes.
 global face_names
 global known_face_encodings
-
 known_face_encodings = [
     leleo_face_encoding,
     bastos_face_encoding
 ]
-
 known_face_names = [
-    "Leonardo Feliciano",
+    "Leleo",
     "Gabriel Bastos"
 ]
 
 # Inicializa algumas variaveis
+
+#rgb_small_frame
+
 face_locations = []
 face_encodings = []
 face_names = []
@@ -61,64 +48,90 @@ process_this_frame = True
 tempo_anterior = 0
 tempo = 0
 
-dataAtual = datetime.now().strftime('%d/%m/%Y')
-
 #################
 # Funcao para usar o multiprocessing do python
 
 def face_match(face_encoding):
-    # Ver se a face encontrada esta entre as conhecidas
-    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    name = "Desconhecido"
+	# Ver se a face encontrada está entre as conhecidas
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Desconhecido"
 
         # Se der um "match" entre a face reconhecida e uma já conhecida, usa a primeira.
-    if True in matches:
+        if True in matches:
+        	first_match_index = matches.index(True)
+        	name = known_face_names[first_match_index]
 
-        first_match_index = matches.index(True)
-        name = known_face_names[first_match_index]
-
-    face_names.append(name)
+        face_names.append(name)
 
 # Funcao para capturar a imagem
 
-global frame
-global ret
-global rgb_small_frame
+def capture_and_resize():
+    global ret
+    global frame
+    global rgb_small_frame
 
-def capture_and_resize(frame, ret, rgb_small_frame):
     ret, frame = video_capture.read() # Captura um frame do video
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25) # Redimensiona a imagem para 1/4 da qualidade original para melhorar o tempo de reconhecimento.
     rgb_small_frame = small_frame[:, :, ::-1] # Converte do padrao BRG (openCV usa) para o RGB (face_recognition usa).
 
+#
+
+def processar_video(rgb_small_frame):
+    global face_names
+    global face_locations
+
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+    face_names = []
+    if __name__ == '__main__':
+	pool = mp.Pool(2)
+	pool.map(face_match, face_encodings)
+	pool.close()
+	pool.join()
+
 #################
+
+global rgb_small_frame
 
 while True:
     antes = time.time()
+    processos = []
+    
 
-    capture_and_resize(frame, ret, rgb_small_frame)
-    """
     # Captura um frame do video
-    ret, frame = video_capture.read()
+    #ret, frame = video_capture.read()
 
     # Redimensiona a imagem para 1/4 da qualidade original para melhorar o tempo de reconhecimento.
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    #small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Converte do padrao BRG (openCV usa) para RGB (face_recognition usa).
-    rgb_small_frame = small_frame[:, :, ::-1]
-    """
+    #rgb_small_frame = small_frame[:, :, ::-1]
+    
+    #capture_and_resize()
+
+    p1 = multiprocessing.Process(target=capture_and_resize)
+    processos.append(p1)
+    p1.start()
     # So processa um frame por vez para economizar tempo
+    
     if process_this_frame:
+        global rgb_small_frame
+        p2 = multiprocessing.Process(target=processar_video, args=rgb_small_frame)
+        processos.append(p2)
+        p2.start()
+        #process()
         # Procura todas as faces no video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        #face_locations = face_recognition.face_locations(rgb_small_frame)
+        #face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        face_names = []
-        if __name__ == '__main__':
+        #face_names = []
+        #if __name__ == '__main__':
+	#	pool = mp.Pool(2)
+	#	pool.map(face_match, face_encodings)
+	#	pool.close()
+	#	pool.join()
 
-            pool = mp.Pool(1)
-            pool.map(face_match, face_encodings)
-            pool.close()
-            pool.join()
     process_this_frame = not process_this_frame
 
 
@@ -129,9 +142,8 @@ while True:
         right *= 4
         bottom *= 4
         left *= 4
-
-
-        if (name == "Desconhecido"):
+        
+        if (name=="Desconhecido"):
             # Desenha uma caixa em torno da pessoa
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
@@ -139,7 +151,7 @@ while True:
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
+        
         else:
             # Desenha uma caixa em torno da pessoa
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
@@ -148,37 +160,25 @@ while True:
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-        
-        if name != "Desconhecido":
-
-            people = session.query(All_people).filter_by(name=name).one()
-            pessoa = session.query(Pessoa).filter_by(nome=name).all()
-
-            for pessoas in pessoa:
-                print (pessoas.hora_chegada)
-
-            if pessoas.data != dataAtual:
-                insert = Pessoa(nome=name, dre = people.dre, data=dataAtual, hora_chegada=datetime.now().strftime('%H:%M:%S'), hora_saida=pessoas.hora_saida)
-                session.add(insert)
-                session.commit()
-            else:
-                session.query(Pessoa).filter_by(nome=name).filter_by(data=dataAtual).update({"hora_saida": datetime.now().strftime('%H:%M:%S')})
-                session.commit()
-
+    
     # Mostra a imagem resultante
     cv2.imshow('Video', frame)
 
     agora = time.time()
     diferenca = (agora - antes)
-    print ("Diferenca:",diferenca)
+    print diferenca
 
     # Aperte 'q' para sair!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Fecha a conexao com a webcam
+
+for processo in processos:
+    processo.join()
+
 agora = time.time()
 diferenca = (agora - antes)
-print (diferenca)
+print diferenca
 video_capture.release()
 cv2.destroyAllWindows()
